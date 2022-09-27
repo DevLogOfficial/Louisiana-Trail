471ce53e5c1a105c6ab1e23e6242696b85169510
Taylor Davis <taylor.p.d94@hotmail.com>
2022-09-27 12:12:35 -0500

    flutter project init

---
+++ android/app/src/main/kotlin/com/example/louisiana_trail/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.louisiana_trail
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- sketch.js
+++
@@ -1,7 +0,0 @@
-function setup() {
-  createCanvas(1000, 800);
-}
-
-function draw() {
-  fill(225);
-}
