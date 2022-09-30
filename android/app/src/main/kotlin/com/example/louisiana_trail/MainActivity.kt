a1cca51d9f92919bc0ef17027d50587b874ef8c8
ShadowLogan31 <48600104+ShadowLogan31@users.noreply.github.com>
2022-09-29 21:40:44 -0500

    Update

--- android/app/src/main/kotlin/com/example/louisiana_trail/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.louisiana_trail
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/team11/louisianatrail/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.team11.louisianatrail
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
